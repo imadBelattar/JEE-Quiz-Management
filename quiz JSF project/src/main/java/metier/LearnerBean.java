75fa7a58139e3d7eb92c7bc18c8f31d56eb95965
imad belattar <imad.belattar.dev@gmail.com>
2023-04-21 19:58:32 +0000

    project creator+learner... 21/04/2023

---
+++ quiz JSF project/src/main/java/metier/LearnerBean.java
@@ -0,0 +1,86 @@
+package metier;
+
+import com.DAO.Quiz;
+import com.DAO.User;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class LearnerBean implements Serializable {
+
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("quiz");
+    private EntityManager entityManager;
+    private List<User> creatorUsers;
+    private List<Quiz> selectedQuizzes;
+
+    public LearnerBean() {
+        creatorUsers = new ArrayList<User>();
+        retrieveCreatorUsers();
+    }
+
+    public List<User> getCreatorUsers() {
+        return creatorUsers;
+    }
+
+    public void setCreatorUsers(List<User> creatorUsers) {
+        this.creatorUsers = creatorUsers;
+    }
+
+    public List<Quiz> getSelectedQuizzes() {
+        return selectedQuizzes;
+    }
+
+    public void setSelectedQuizzes(List<Quiz> selectedQuizzes) {
+        this.selectedQuizzes = selectedQuizzes;
+    }
+
+    public void retrieveCreatorUsers(){
+        entityManager = entityManagerFactory.createEntityManager();
+        try {
+            String jpql = "SELECT u FROM User u where u.role = 1";
+            TypedQuery<User> query = entityManager.createQuery(jpql, User.class);
+            creatorUsers = query.getResultList();
+            for(User user : creatorUsers){
+                jpql = "SELECT q FROM Quiz q where q.user.id = :user_d";
+                TypedQuery<Quiz> query2 = entityManager.createQuery(jpql,Quiz.class);
+                query2.setParameter("user_id", user.getId());
+                user.setQuizzes(query2.getResultList());
+            }
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Error retrieving creatorUsers"));
+        } finally {
+            entityManager.close();
+        }
+    }
+    public void selectQuizzes(List<Quiz> list){
+        this.selectedQuizzes = list;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //end of class
+}
